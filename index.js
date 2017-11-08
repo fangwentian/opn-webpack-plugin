31f9e535343a5980143f9ee995d73ddfce1fda91
hzfangwentian <hzfangwentian@corp.netease.com>
2017-11-08 16:01:42 +0800

    update

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const opn = require('opn');
 const _ = require('lodash');
 
 // opnOptions：传给opn的配置
-function OpnPlugin(opnOptions) {
+function OpnWebpackPlugin(opnOptions) {
     this.options = _.extend({
         url: 'http://127.0.0.1',
         port: '8000',
     }, opnOptions)
 }
 
-OpnPlugin.prototype.apply = function(compiler) {
+OpnWebpackPlugin.prototype.apply = function(compiler) {
     compiler.plugin('compile', function(params) {
         console.log('The compiler is starting to compile...', params)
     });
-    
 }
+
+module.exports = OpnWebpackPlugin;
